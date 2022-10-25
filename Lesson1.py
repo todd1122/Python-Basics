5581b2be1cc5193ab8e55666616650f1106a23d3
Todd Kaduck <toddkaduck84@gmail.com>
2022-10-24 23:31:54 -0400

    Lesson1.py added

---
+++ Lesson1.py
@@ -0,0 +1,14 @@
+charspace = " "
+charunderscore = "_"
+charvertline = "|"
+charforwardslash = "/"
+print(charunderscore * 5)
+print(charvertline + charspace * 3 + charforwardslash)
+print(charvertline + charspace * 2 + charforwardslash)
+print(charvertline + charspace * 1 + charforwardslash)
+print(charvertline + charspace * 0 + charforwardslash)
+
+
+
+
+
