41f6afe4e7c59f2b5ba1e57d34d9677e42f4951a
Todd Kaduck <toddkaduck84@gmail.com>
2022-10-25 00:11:55 -0400

    Lesson2.py added

---
+++ Lesson2.py
@@ -0,0 +1,15 @@
+my_string = "Test Automation"
+word_test = my_string[:4]
+print(word_test)
+
+my_string = "Test Automation"
+second_word = my_string[5:]
+print(second_word)
+
+
+my_list = [10,50,20,25,30]
+sliced_list = my_list[1:3]
+print(sliced_list)
+
+
+
